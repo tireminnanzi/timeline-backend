008cdfeb150e67c26e95fa09c559d4039dffbc73
tireminnanzi <tireminnanzi@gmail.com>
2025-03-09 00:54:16 +0100

    Added debug logs to /movies endpoint

--- index.js
+++ index.js
@@ -41,10 +41,14 @@ db.serialize(() => {
   });
 });
 app.get('/movies', (req, res) => {
+  console.log('Received request for /movies');
   db.all("SELECT * FROM movies", [], (err, rows) => {
+    console.log('Query error:', err);
+    console.log('Rows:', rows);
     if (err) res.status(500).json({ error: err.message });
     else res.json(rows);
   });
+});
 });
 // API endpoint to get all movies
 //app.get('/movies', (req, res) => {
