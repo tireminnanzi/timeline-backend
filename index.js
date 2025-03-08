a1d43376d3b100b8183f3a5f28a2dbcc18fd94af
tireminnanzi <tireminnanzi@gmail.com>
2025-03-09 00:20:07 +0100

    Added debug logs to /movies endpoint

--- index.js
+++ index.js
@@ -54,6 +54,10 @@ app.get('/movies', (req, res) => {
 //  });
 // });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
 });
+
+//app.listen(port, () => {
+//  console.log(`Server running on port ${port}`);
+//});
