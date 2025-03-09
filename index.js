63b5d44803a85861622e663fb718a7f6c1b76483
tireminnanzi <tireminnanzi@gmail.com>
2025-03-09 13:37:32 +0100

    Resolved merge conflicts

--- index.js
+++ index.js
@@ -22,18 +22,19 @@ db.serialize(() => {
       title TEXT NOT NULL,
       yearStart INTEGER NOT NULL,
       yearEnd INTEGER NOT NULL,
-      releaseYear INTEGER
+      releaseYear INTEGER,
+      importance REAL
     )
   `);
 
   db.get("SELECT COUNT(*) as count FROM movies", (err, row) => {
     if (row.count === 0) {
       db.run("BEGIN TRANSACTION");
-      db.run("INSERT INTO movies (title, yearStart, yearEnd, releaseYear) VALUES (?, ?, ?, ?)", ["Back to the Future", 1985, 1985, 1985]);
-      db.run("INSERT INTO movies (title, yearStart, yearEnd, releaseYear) VALUES (?, ?, ?, ?)", ["Back to the Future", 1955, 1955, 1985]);
-      db.run("INSERT INTO movies (title, yearStart, yearEnd, releaseYear) VALUES (?, ?, ?, ?)", ["Back to the Future", 2015, 2015, 1985]);
-      db.run("INSERT INTO movies (title, yearStart, yearEnd, releaseYear) VALUES (?, ?, ?, ?)", ["Gladiator", 180, 192, 2000]);
-      db.run("INSERT INTO movies (title, yearStart, yearEnd, releaseYear) VALUES (?, ?, ?, ?)", ["The Time Machine", 802701, 802701, 1960], () => {
+      db.run("INSERT INTO movies (title, yearStart, yearEnd, releaseYear, importance) VALUES (?, ?, ?, ?, ?)", ["Back to the Future", 1985, 1985, 1985, 38.5]);
+      db.run("INSERT INTO movies (title, yearStart, yearEnd, releaseYear, importance) VALUES (?, ?, ?, ?, ?)", ["Back to the Future", 1955, 1955, 1985, 38.5]);
+      db.run("INSERT INTO movies (title, yearStart, yearEnd, releaseYear, importance) VALUES (?, ?, ?, ?, ?)", ["Back to the Future", 2015, 2015, 1985, 38.5]);
+      db.run("INSERT INTO movies (title, yearStart, yearEnd, releaseYear, importance) VALUES (?, ?, ?, ?, ?)", ["Gladiator", 180, 192, 2000, 43.5]);
+      db.run("INSERT INTO movies (title, yearStart, yearEnd, releaseYear, importance) VALUES (?, ?, ?, ?, ?)", ["The Time Machine", 802701, 802701, 1960, 24.5], () => {
         db.run("COMMIT", () => {
           console.log("Inserted initial data with year ranges");
           db.all("SELECT * FROM movies", [], (err, rows) => {
